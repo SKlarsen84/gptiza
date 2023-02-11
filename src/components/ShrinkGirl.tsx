5664ab65eca75ce481e2b6d7a52b7446392cb1c2
Stefan Larsen <stefan.larsen@northstake.dk>
2023-02-11 13:25:36 +0100

    t

--- src/components/ShrinkGirl.tsx
+++ src/components/ShrinkGirl.tsx
@@ -395,6 +395,28 @@ function updateWindowSize() {
 }
 updateWindowSize();
 window.addEventListener("resize", updateWindowSize);
+
+function enableAutoTTS() {
+  if (typeof window === 'undefined') {
+    return;
+  }
+  const isiOS = navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+  if (!isiOS) {
+    return;
+  }
+  const simulateSpeech = () => {
+    const lecture = new SpeechSynthesisUtterance('hello');
+    lecture.volume = 0;
+    speechSynthesis.speak(lecture);
+    document.removeEventListener('click', simulateSpeech);
+  };
+
+  document.addEventListener('click', simulateSpeech);
+}
+
+enableAutoTTS();
+
+
           
          `}</Script>
 
